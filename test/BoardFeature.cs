01899ba0f6740584e7545c7011e76b2a8110ed40
ambsenyestudi <adrifre7@hotmail.com>
2022-09-27 08:25:42 +0200

    Comments

--- test/BoardFeature.cs
+++ test/BoardFeature.cs
@@ -5,6 +5,7 @@ namespace Sudoku.Test;
 public class BoardFeature
 {
     private readonly Board _board = new Board();
+    //evaluate if valid solution for board
     [Fact]
     public void Test1()
     {

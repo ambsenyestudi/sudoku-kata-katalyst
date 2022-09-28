625c8bc88689cb3b0c8700ea7c0fdc0a33174f17
ambsenyestudi <adrifre7@hotmail.com>
2022-09-28 08:08:50 +0200

    Extract method refactor

--- src/infrastructure/GameService.cs
+++ src/infrastructure/GameService.cs
@@ -18,12 +18,15 @@ namespace Sudoku.Infrastructure
         public bool IsSolution(Board board, string solutionPath)
         {
             var solution = _fileLoader.LoadBoard(solutionPath);
-            if(solution.Any(x => EmptyCell.IsEmptyValue(0)) )
+            if (HasEmptyCells(solution))
             {
                 return false;
             }
-            
+
             throw new NotImplementedException();
         }
+
+        private static bool HasEmptyCells(int[] solution) =>
+            solution.Any(x => EmptyCell.IsEmptyValue(0));
     }
 }
